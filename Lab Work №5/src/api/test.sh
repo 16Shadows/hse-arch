c2a0cee6dab04276fa071107919c1bbd7026672c
16Shadows <egor.0.00@mail.ru>
2025-02-16 17:24:02 +0500

    ...

--- Lab Work №5/src/api/test.sh
+++ Lab Work №5/src/api/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ curl -XGET http://localhost/teos -H "Authorization: User user" -f ]; then
+if [ (curl -XGET http://localhost/teos -H "Authorization: User user" -f) ]; then
 	exit 1
 fi
 
-if ! [ curl -XGET http://localhost/teos -H "Authorization: User test" -f ]; then
+if ! [ (curl -XGET http://localhost/teos -H "Authorization: User test" -f) ]; then
 	exit 1
 fi
 
